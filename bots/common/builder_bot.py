d3ed6957a1d9f728dbc6dc6e8064f4a2c9b54d98
dragonier23 <84836280+dragonier23@users.noreply.github.com>
2026-03-18 00:43:37 +0000

    feat: get environment, converyer belt planning

--- bots/common/builder_bot.py
+++ bots/common/builder_bot.py
@@ -1,6 +1,7 @@
-from cambc import Controller, Environment, Position, EntityType
+from cambc import Controller, Environment, Position, EntityType, Direction
+from collections import deque
 from typing import List
-from const import TileState
+from const import TileState, BuildingState, DirectedBuildings
 
 class BuilderBot:
     def __init__(self):
@@ -25,6 +26,27 @@ class BuilderBot:
                 if 0 <= x < self.w and 0 <= y < self.h:
                     self.tile_env[x][y] = ct.get_tile_environment(Position(x, y))
 
+        for pos in ct.get_nearby_tiles(): 
+            x, y = pos.x, pos.y
+
+            tile_env = ct.get_tile_env(pos)
+            if self.grid[x][y] is None:
+                match tile_env:
+                    case Environment.WALL:
+                        self.grid[x][y] = TileState(Environment.WALL)
+                    case Environment.ORE_AXIONITE | Environment.ORE_TITANIUM | Environment.EMPTY:
+                        self.grid[x][y] = TileState(tile_env)
+                    case Environment.EMPTY: 
+                        self.grid[x][y] = TileState(Environment.EMPTY)
+
+            entity_id = ct.get_tile_building_id(pos)
+            if entity_id:
+                self.grid[x][y].team = ct.get_team(entity_id)
+                entity_type = ct.get_entity_type(entity_id)
+                direction = ct.get_direction(entity_id) if entity_type.name in DirectedBuildings.__members__ else None
+                bot = True if ct.get_tile_builder_bot_id(pos) else False
+                self.grid[x][y].building = BuildingState(entity_type, direction, bot)
+
     def run(self, ct: Controller) -> None:
         if not self.initialised:
             self.init(ct)
@@ -33,3 +55,28 @@ class BuilderBot:
     def get_direction(self, goal):
         pass
 
+    def conveyor_path(self, ct: Controller, pos: Position) -> List[Position] | None:
+        """
+        Return the shortest path from pos to any tile around the 3x3 core.
+        Currently just a BFS search, preferably do something slightly more efficient
+        """
+
+        q = deque([pos])
+        marked = {pos: None}
+
+        while q: 
+            c = q.popleft()
+            if c.x in range(self.core_pos.x - 2, self.core_pos.x + 3) and c.y in range(self.core_pos.y - 2, self.core_pos.y + 3): 
+                res = deque([c])
+                while c != pos: 
+                    res.appendleft(marked[c])
+                    c = marked[c]
+                return res                 
+
+            for d in Direction:
+                neighbour = c.add(d)
+                if neighbour not in marked: 
+                    tile = self.grid[neighbour.x][neighbour.y]
+                    if tile.env == Environment.EMPTY and tile.building in [None, EntityType.ARMOURED_CONVEYOR, EntityType.BUILDER_BOT, EntityType.CONVEYOR, EntityType.ROAD, EntityType.SPLITTER, EntityType.BRIDGE]: 
+                        marked[neighbour] = c
+                        q.append(neighbour)
--- bots/common/const.py
+++ bots/common/const.py
@@ -1,6 +1,18 @@
 from enum import Enum
 from cambc import Environment, EntityType, Team, Direction
 
+class DirectedBuildings(Enum): 
+    __slot__ = () 
+
+    GUNNER = "gunner"
+    SENTINEL = "sentinel"
+    BREACH = "breach"
+    LAUNCHER = "launcher"
+    CONVEYOR = "conveyor"
+    SPLITTER = "splitter"
+    ARMOURED_CONVEYOR = "armoured_conveyor"
+    BRIDGE = "bridge"
+
 class BuilderState(Enum): 
     __slot__ = ()
 
@@ -8,18 +20,24 @@ class BuilderState(Enum):
     CONVEY = "convey"
 
 class BuildingState: 
-    def __init__(self, type: EntityType, direction: Direction): 
+    def __init__(self, type: EntityType, direction: Direction, bot: bool = False): 
         self.type = type 
         self.direction = direction
+        self.bot = bot
+
+class MarkerState: 
+    def __init__(self, message): 
+        self.message = message
+
 class TileState: 
     '''
-    record the state of a particular tile: 
-    Should record the type Environment state - Empty, Wall, Ore type
-    if it is empty, it could contain something - what entity is in it, or is it empty
-    we want to also record if it is friendly or not 
-    we want to record if it is a weapon, some stats
-    we want to record if its a converyer, the direction 
+    Records the state of a particular tile: 
+    Team: the team the tile belongs to 
+    Environment: Empty, Wall, Ore type
+    Building: Contains the object type and the direction it is facing. 
     '''
-    def __init__(self, team: Team, env: Environment , building: BuildingState): 
+    def __init__(self, env: Environment, team: Team = None, building: BuildingState = None, marker: MarkerState = None): 
         self.team = team 
         self.env = env
+        self.building = building
+        self.marker = marker
